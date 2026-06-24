86d7194cdf7ae72064f929af15e7fbb55840c64f
shashankreddyvadyala <44911124+shashankreddyvadyala@users.noreply.github.com>
2026-06-24 13:15:02 -0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,1 +1,3 @@
-
+The table or view `31500_atims_dev`.`atims_taxability`.`prediction_ready` cannot be found. Verify the spelling and correctness of the schema and catalog.
+If you did not qualify the name with a schema, verify the current_schema() output, or qualify the name with the correct schema and catalog.
+To tolerate the error on drop use DROP VIEW IF EXISTS or DROP TABLE IF EXISTS. SQLSTATE: 42P01
