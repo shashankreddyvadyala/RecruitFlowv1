7eedf9906a97edcab79a1ee76b92011fc7312f57
shashankreddyvadyala <44911124+shashankreddyvadyala@users.noreply.github.com>
2026-08-31 11:17:06 -0400

    Change error message for missing table/view
    Updated error message for table/view not found.

--- App.tsx
+++ App.tsx
@@ -1,1 +1,4 @@
-[NOT_SUPPORTED_WITH_SERVERLESS] PERSIST TABLE is not supported on serverless compute. SQLSTATE: 0A000
+[TABLE_OR_VIEW_NOT_FOUND] The table or view `31500_atims_dev`.`atims_taxability`.`t32_rule_audit` cannot be found. Verify the spelling and correctness of the schema and catalog.
+Search path: [`system`.`session`, `system`.`builtin`, `system`.`ai`, `hive_metastore`.`default`].
+If you did not qualify the name with a schema, verify the current_schema() output, or qualify the name with the correct schema and catalog.
+To tolerate the error on drop use DROP VIEW IF EXISTS or DROP TABLE IF EXISTS. SQLSTATE: 42P01
