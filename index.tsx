3d81f86e1adf68b7de4de4c35f488ef2b35259e6
shashankreddyvadyala <44911124+shashankreddyvadyala@users.noreply.github.com>
2026-09-08 07:21:21 -0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,1 +1,3 @@
-
+'PO_NUMBER', 'INVOICE_ID', 'INVOICE_NUM', 'SOURCE', 'INV_LINE_NUMBER', 'ACCOUNT_CODE', 'COMPANY_CODE', 'MIC', 'INVOICE_DATE', 'INVOICE_AMOUNT', 'TRAN_AMOUNT', 'EXTC', 'STATE', 'VENDOR_NAME', 'INVOICE_FR', 'FINAL_DESCRIPTION', 'PROJ_NUM', 'TAX_AMOUNT', 'TAX_TYPE', 'ACCOUNTING_DATE', 'NORAD_FLAG', 'DRIVERID'
+1 Like reaction.,,Please include all of these along with the use tax amounts and other ml ouput columns
+1 Heart reaction.
